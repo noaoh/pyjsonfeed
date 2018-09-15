455e4dca50911677275e2ddd174f916c74954df8
Unknown <noahryanholt@gmail.com>
2018-09-15 14:41:25 -0500

    Added license, travis CI & packaging

---
+++ setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+setuptools.setup(
+    name='pyjsonfeed',
+    version='1.0.0',
+    packages=setuptools.find_packages(),
+    url='https://github.com/noaoh/pyjsonfeed',
+    license='MIT',
+    author='noaoh',
+    author_email='noahryanholt@gmail.com',
+    description='A package for parsing json feeds inspired by feedparser'
+)
